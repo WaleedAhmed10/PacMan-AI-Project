c9441631f6679ee1256bd6d0941b633cc3072f25
Waleed Ahmed <146324731+WaleedAhmed10@users.noreply.github.com>
2026-05-09 11:18:50 +0500

    utility functions and sounds comitted

---
+++ PacMan Project/soundManager.py
@@ -0,0 +1,28 @@
+class soundManager:
+    def __init__(self):
+        self.sounds = {
+        "chomp"      : load_sound("chomp.wav"),
+        "power_up"   : load_sound("power_pellet.wav"),
+        "ghost_eat"  : load_sound("ghost_eat.wav"),
+        "death"      : load_sound("pacman_death.wav"),
+        "fruit"      : load_sound("fruit.wav"),
+        "game_start" : load_sound("game_start.wav"),
+        "extra_life" : load_sound("extra_life.wav"),
+        "siren"      : load_sound("siren.wav"),
+        "retreating" : load_sound("retreating.wav"),
+    }
+    self.siren_channel = None
+    def play(self, sound_name):
+        if sound_name == "siren":
+            self.sounds[name].play(loops = -1 if loop else 0)
+    def stop(self, sound_name):
+        if sound_name == "siren" and self.siren_channel is not None:
+            self.siren_channel.stop()
+    def update(self, game_state):
+        if game_state == "chase":
+            self.play("siren")
+        else:
+            self.stop("siren")
+            self.play("retreating")
+    def load_sound(filename):
+        pass
