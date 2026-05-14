83eafb76a347b5addcf61c0296a99e12897ff00d
Waleed Ahmed <146324731+WaleedAhmed10@users.noreply.github.com>
2026-05-15 00:51:46 +0500

    input_handler corrected

--- PacMan Project/input_handler.py
+++ PacMan Project/input_handler.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import pygame
-from constraints import GAME_OVER, PAUSED, PLAYING, START
+from constants import GAME_OVER, PAUSED, PLAYING, START, UP, DOWN, LEFT, RIGHT
+
 
 class InputHandler:
     def handle_events(self, game_state_manager, pacman, sound=None):
@@ -9,15 +9,18 @@ class InputHandler:
                 game_state_manager.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key in (pygame.K_UP, pygame.K_w):
-                    pacman.direction = 'up'
+                    pacman.next_direction = UP
                 elif event.key in (pygame.K_DOWN, pygame.K_s):
-                    pacman.direction = 'down'
+                    pacman.next_direction = DOWN
                 elif event.key in (pygame.K_LEFT, pygame.K_a):
-                    pacman.direction = 'left'
+                    pacman.next_direction = LEFT
                 elif event.key in (pygame.K_RIGHT, pygame.K_d):
-                    pacman.direction = 'right'
+                    pacman.next_direction = RIGHT
                 elif event.key in (pygame.K_ESCAPE, pygame.K_q):
-                    game_state_manager.state = PAUSED if game_state_manager.state == PLAYING else PLAYING
+                    if game_state_manager.state == PLAYING:
+                        game_state_manager.state = PAUSED
+                    elif game_state_manager.state == PAUSED:
+                        game_state_manager.state = PLAYING
                 elif event.key in (pygame.K_RETURN, pygame.K_SPACE):
                     if game_state_manager.state == START:
                         game_state_manager.state = PLAYING
@@ -25,34 +28,3 @@ class InputHandler:
                         game_state_manager.restart()
                 elif event.key == pygame.K_m and sound:
                     sound.toggle_mute()
-=======
-from json.encoder import ESCAPE
-from constraints import GAME_OVER, PAUSED, PLAYING, START
-
-
-class inputHandler:
-    def handle_events(self, game_state_manager, pacman):
-        for event in game_state_manager.events:
-            if event.type == game_state_manager.QUIT:
-                game_state_manager.running = False
-            elif event.type == game_state_manager.KEYDOWN:
-                if event.key == game_state_manager.K_UP:
-                    pacman.direction = 'up' or pacman.direction = 'W'
-                elif event.key == game_state_manager.K_DOWN:
-                    pacman.direction = 'down' or pacman.direction = 'S'
-                elif event.key == game_state_manager.K_LEFT:
-                    pacman.direction = 'left' or pacman.direction = 'A'
-                elif event.key == game_state_manager.K_RIGHT:
-                    pacman.direction = 'right' or pacman.direction = 'D'
-            if event.key == ESCAPE or event.key == game_state_manager.K_q:
-                if game_state_manager.state == PLAYING:
-                    game_state_manager.state = PAUSED
-                elif game_state_manager.state == PAUSED:
-                    game_state_manager.state = PLAYING
-            if event.key == ENTER or event.key == game_state_manager.SPACE:
-                if game_state_manager.state == START:
-                    game_state_manager.state = PLAYING
-                elif game_state_manager.state == GAME_OVER:
-                    game_state_manager.restart()
-                if event.key == game_state_manager.K_m: sound.toggle_mute()
->>>>>>> c9441631f6679ee1256bd6d0941b633cc3072f25
